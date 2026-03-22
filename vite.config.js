220d2569f9db04701d9775a4d6a1fd7b7fd315c2
Anirudh Seshadri <aseshadri30@gatech.edu>
2026-03-22 14:43:40 -0700

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/anirudh-portfolio/',
+  base: '/portfolio-website/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
